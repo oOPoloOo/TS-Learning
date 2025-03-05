c74cc6f43f2ae2b3ece5fa6e43a7315b3d0f8566
Paulius <p.jacikeviciusinfo@gmail.com>
2025-03-05 14:56:39 +0200

    Problem: ColorArray type interupts recursion

--- Task 6/script.ts
+++ Task 6/script.ts
@@ -14,3 +14,50 @@ Pvz.:
   new_potion.color  =  [127, 159, 127]
   new_potion.volume =  19
 ------------------------------------------------------------------------------------------------------ */
+
+type NumericRange
+<
+    START extends number,
+    END extends number,
+    ARR extends unknown[] = [],
+    ACC extends number = never
+> 
+= ARR['length'] extends END
+    ? ACC | START | END
+    : NumericRange<START, END, [...ARR, 1], ARR[START] extends undefined ? ACC : ACC | ARR['length']>
+
+type ColorNumber = NumericRange<0,255>;
+type ColorArray = [ColorNumber, ColorNumber, ColorNumber];
+
+
+// Gal taip geriau butu, bet konstruktoriai pagavo.
+// type MixedArrayType = ColorNumber | number[];
+
+class Potion {
+  color?: number[];
+  colorChecked?: ColorArray;
+  volume: number;
+
+  constructor( volume: number, color?: number[], colorChecked?: ColorArray)
+  {    
+    if(colorChecked) this.colorChecked = colorChecked;  
+    if(color) this.color = color;    
+    this.volume = volume;
+  } 
+ 
+  mix(ingridients: Potion): Potion {
+    const newVolume = this.volume + ingridients.volume;
+
+    const newColor = this.color.map((c, i) =>
+      Math.round((c * this.volume + ingridients.color[i] * ingridients.volume) / newVolume)
+    );
+    
+    return new Potion(newColor, newVolume);
+  }
+ 
+}
+
+// Example usage
+const felixFelicis = new Potion([255, 255, 255], 7);
+const polyjuice = new Potion([51, 102, 51], 12);
+const newPotion = felixFelicis.mix(polyjuice);
